766d6067fccc4b776bd7f71feb4b108cf1a52767
Faisal <muhammadfaisalmalik01@gmail.com>
2026-07-09 14:23:29 +0530

    add resume-v2.pdf

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -20,7 +20,7 @@ I'm actively seeking software engineering internships where I can contribute, le
   github: "https://github.com/Faisalmalik01",
   linkedin: "https://www.linkedin.com/in/faisalmalik01/",
   twitter: "https://x.com/faisalmalik_01",
-  resume: "/resume.pdf",
+  resume: "/resume-v2.pdf",
 };
 
 export const education = [
